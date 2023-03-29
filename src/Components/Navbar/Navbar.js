6f1d9379e1a4bf22e3b24728ff1170eb7b7d04e6
yannickwnz <yannickwnz@gmail.com>
2023-03-29 17:56:25 +0000

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,8 +3,11 @@ import './Navbar.scss'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
-function Navbar() {
+function Navbar({change_menu_function, menu_state}) {
     return (
+
+
+
         <div id='navbar'>
             <div className='navbar-container'>
                 <div className='navbar-contents-wrapper'>
@@ -12,13 +15,13 @@ function Navbar() {
                         <Link to='/' ><img src='/assets/logo.svg' /></Link>
                     </div>
                     <div className='navbar-links-wrapper'>
-                        <ul className='nav-links'>
+                        <ul className={`nav-links ${menu_state ? 'show-menu' : ''}`}>
                             <li className='nav-list'><Link to='/portfolio' >Portfolio</Link></li>
                             <li className='nav-list'><Link to='/about' >About Us</Link></li>
                             <li className='nav-list'><Link to='/contact' >Contact</Link></li>
                         </ul>
                     </div>
-                    <i className="fa-solid fa-bars"></i>
+                    <i className="fa-solid fa-bars" onClick={change_menu_function}></i>
                 </div>
             </div>
         </div>
