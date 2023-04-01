3e50b583ac358ed14d1a2c98cf53e51acaf0173e
yannickwnz <yannickwnz@gmail.com>
2023-04-01 11:50:09 +0000

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Footer from '../../Components/Footer/Footer'
 import './Home.scss'
 
 function Home() {
@@ -19,7 +20,7 @@ function Home() {
       if(currentIdx == 3) {
         setCurrentIdx(0)
       }
-    }, 5000)
+    }, 10000)
     return () => clearInterval(interval)
   })
   
@@ -56,46 +57,6 @@ function Home() {
               </div>
             </div>
             })}
-            {/* <div className='slide1 slide'>
-              <div className='slide-text-container'>
-                <h1>Project Paramour</h1>
-                <p>Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.</p>
-                <div className='btn-wrapper'>
-                  <Link>See our portfolio </Link>
-                  <i className="fa-solid fa-arrow-right"></i>
-                </div>
-              </div>
-            </div>
-            <div className='slide2 active slide'>
-              <div className='slide-text-container'>
-                  <h1>Seraph Station</h1>
-                  <p>The Seraph Station project challenged us to design a unique station that would transport people throughh time. The result is a fresh and futuristic model inspired by space stations.</p>
-                  <div className='btn-wrapper'>
-                    <Link>See our portfolio </Link>
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-              </div>
-            </div>
-            <div className='slide3  slide'>
-              <div className='slide-text-container'>
-                  <h1>Federal II Tower</h1>
-                  <p>A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.</p>
-                  <div className='btn-wrapper'>
-                    <Link>See our portfolio </Link>
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-              </div>
-            </div>
-            <div className='slide4  slide'>
-              <div className='slide-text-container'>
-                  <h1>Trinity Bank Tower</h1>
-                  <p>Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.</p>
-                  <div className='btn-wrapper'>
-                    <Link>See our portfolio </Link>
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </div>
-              </div>
-            </div> */}
             <div className='switching-btn-wrapper'>
             {switchingNumbers.map((number, index) => {
               return <div key={index} onClick={() => goToSlide(index)} className={index === currentIdx ? 'number active' : 'number'}>
@@ -103,23 +64,89 @@ function Home() {
                     </div>
             })}
             </div>
-            {/* <div className='switching-btn-wrapper'>
-              <div className='one active' onClick={() => {
-                testGoToSlide(1)
-              }}>
-                <p>1</p>
-              </div>
-              <div className='two '>
-                <p>2</p>
-              </div>
-              <div className='three'>
-                <p>3</p>
-              </div>
-              <div className='four'>
-                <p>4</p>
-              </div>
-            </div> */}
           </div>
+          {/* welcome to arch studio section START */}
+          <section className='welcome'>
+            <div className='welcome-contents'>
+              <h1>Welcome</h1>
+              <hr />
+              <div className="contents-wrapper">
+                <h2>Welcome to Arch Studio</h2>
+                <p>
+                  We have a unique network and skillset to help bring your projects to life. Our small team of highly skilled individuals combined with our large network put us in a strong position to deliver exceptional results. <br /> <br/>
+                  Over the past 10 years, we have workd on all kinds of projects. From stations to high-rise buildings, we create spaces that inspire and delight. <br /> <br />
+                  We work closely with our client so that we understand the intricacies of each project. Thid allows us to work in harmany the surrounding area to create truly stunning projects that will stand the test of time.
+                </p>
+              </div>
+            </div>
+            <div className='welcome-img'></div>
+          </section>
+          {/* welcome to arch studio section END */}
+          {/* small team big ideas section START */}
+          <section className='home-about'>
+            <div className="home-about-container">
+              <h1>Small teams, big ideas</h1>
+              <div className='btn-wrapper'>
+                <Link to='/about'>About us</Link>
+                <i className="fa-solid fa-arrow-right"></i>
+              </div>
+            </div>
+          </section>
+          {/* small team big ideas section END */}
+          {/* featured project section START */}
+          <section className='featured-projects'>
+            <div className='featured-projects-container'>
+              <div className='intro'>
+                <h1>Featured</h1>
+                <Link className='btn' to='/portfolio'>See all <i className="fa-solid fa-arrow-right"></i></Link>
+              </div>
+              <div className='projects'>
+                {/* <Link> */}
+                  <div className='project-wrapper'>
+                    <div className='number'>
+                      <h1>1</h1>
+                    </div>
+                    <div className='details'>
+                      <p>Project Del Sol</p>
+                      {/* <p>View all projects</p> */}
+                      <Link to='/portfolio'>View all projects</Link>
+                    </div>
+                  </div>
+                {/* </Link> */}
+                {/* <Link> */}
+                <div className='project-wrapper'>
+                  <div className='number'>
+                    <h1>2</h1>
+                  </div>
+                  <div className='details'>
+                    <p>228b Tower</p>
+                    {/* <p>View all projects</p> */}
+                    <Link to='/portfolio'>View all projects</Link>
+                  </div>
+                </div>
+                {/* </Link> */}
+                {/* <Link> */}
+                <div className='project-wrapper'>
+                  <div className='number'>
+                    <h1>3</h1>
+                  </div>
+                  <div className='details'>
+                    <p>Le Prototype</p>
+                    {/* <p>View all projects</p> */}
+                    <Link to='/portfolio'>View all projects</Link>
+                  </div>
+                </div>
+                {/* </Link> */}
+              </div>
+
+              <Link className='btn see-more-portfolio' to='/portfolio'>See all <i className="fa-solid fa-arrow-right"></i></Link>
+                  
+
+            </div>
+          </section>
+          {/* featured project section END */}
+          {/* <div className='testingdiv'></div> */}
+          {/* <Footer /> */}
         </div>
       </div>
     </div>
