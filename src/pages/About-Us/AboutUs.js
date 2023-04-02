00c44df8d2d05d57f95bd534215cffc1f6ca1706
yannickwnz <yannickwnz@gmail.com>
2023-04-02 10:20:36 +0000

    about

--- src/pages/About-Us/AboutUs.js
+++ src/pages/About-Us/AboutUs.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import HeroSection from '../../Components/HeroSection/HeroSection'
+import './AboutUs.scss'
 
 function AboutUs() {
+
+  const page_name = 'About';
+  let title = 'Your team of professionals';
+  let description = 'Our small team o world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationships our projects have with their surroundings.';
+  let vertical_name = 'ABOUT US'
+
   return (
-    <div>AboutUs</div>
+    // <div className='about'>
+    //   <div className='about-container'>
+    //     <div className='about-contents-wrapper'>
+    //       <div className='about-intro'>
+    //         <div className='img-intro'></div>
+    //         <div className='contents-intro'>
+    //           <h1>About</h1>
+    //           <div className='text-content'>
+    //             <hr />
+    //             <h2>Your team of professionals</h2>
+    //             <p>Our small team o world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationships our projects have with their surroundings.</p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className='about'>
+      <HeroSection pageName={page_name} title={title} description={description} vertical_name={vertical_name} /> 
+
+    </div>
+
   )
 }
 
